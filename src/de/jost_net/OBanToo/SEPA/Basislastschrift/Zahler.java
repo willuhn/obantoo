157ae8b88a44ac3c30a55124aed92dfdca466ff0
nils <nils@lenias.ch>
2023-11-06 22:38:50 +0100

    Negative Abbuchungsbetraege zulassen (solange sie nicht zu insgesamt nagativen Abbuchungsbeträgen führen)

--- src/de/jost_net/OBanToo/SEPA/Basislastschrift/Zahler.java
+++ src/de/jost_net/OBanToo/SEPA/Basislastschrift/Zahler.java
@@ -227,7 +227,8 @@ public class Zahler
 
   public void checkBetrag(BigDecimal betrag) throws SEPAException
   {
-    if (betrag == null || betrag.compareTo(nu) == -1
+    if (betrag == null 
+    	//|| (betrag.compareTo(nu) == -1) //Negative erlauben
         || betrag.compareTo(nu) == 0)
     {
       throw new SEPAException("Ungültiger Betrag: " + betrag);
@@ -308,6 +309,8 @@ public class Zahler
       verwendungszweck += " " + this.getBetrag();
     }
     betrag = betrag.add(zahler.getBetrag());
+    if(betrag.compareTo(new BigDecimal("0.00")) == -1)
+    	throw new SEPAException("Ungültiger Betrag: " + betrag);
     if (verwendungszweck.length() == 140 && verwendungszweck.endsWith("..."))
     {
       return;
